b0227a4307e37798c70c0467169a0e95d09afca8
yankyhermawan <yankyhermawan42@gmail.com>
2023-09-12 23:42:06 +0700

    Refactor Card component to fetch and display Pokemon data

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,63 @@
-export default function Card({ name }: { name: string }) {
+import { useState, useEffect } from "react";
+export default function Card({ name, url }: { name: string; url: string }) {
+	const [bgColor, setBGColor] = useState("");
+	const [id, setID] = useState(0);
+	const [bgURL, setBGURL] = useState("");
+	const typeColor: Record<string, string> = {
+		normal: "#aa9",
+		fire: "#f42",
+		water: "#39f",
+		electric: "#fc3",
+		grass: "#7c5",
+		ice: "#6cf",
+		fighting: "#b54",
+		poison: "#a59",
+		ground: "#db5",
+		flying: "#89f",
+		psychic: "#f59",
+		bug: "#ab2",
+		rock: "#ba6",
+		ghost: "#66b",
+		dragon: "#76e",
+		dark: "#754",
+		steel: "#aab",
+		fairy: "#e9e",
+	};
+
+	useEffect(() => {
+		// Initialize the ID when the component mounts
+		setID(Number(url.split("/")[url.split("/").length - 2]));
+	}, [url]);
+
+	useEffect(() => {
+		// Fetch the type and set the background URL and color
+		const getID = async () => {
+			const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+			const data = await response.json();
+			setID(Number(data.id));
+			setBGURL(
+				`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`
+			);
+			setBGColor(typeColor[data.types[0].type.name]);
+		};
+		getID();
+	}, [id, name]);
+
 	return (
 		<>
-			<div className="flex flex-col rounded-lg border-solid border-[1px] border-black hover:bg-orange-200 cursor-pointer duration-200">
+			<a
+				className="flex flex-col rounded-lg border-solid border-[1px] border-black hover:bg-orange-200 cursor-pointer duration-200 w-48 h-72"
+				style={{
+					backgroundColor: `${bgColor}`,
+					backgroundImage: `url(${bgURL})`,
+					backgroundPosition: "center",
+					backgroundSize: "cover",
+					backgroundRepeat: "no-repeat",
+				}}
+				href={`https://pokeapi.co/api/v2/pokemon/${id}`}
+			>
 				<span className="text-lg p-2">{name}</span>
-			</div>
+			</a>
 		</>
 	);
 }
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,8 +3,8 @@ import Card from "./Card";
 
 interface PokeApiData {
 	count: number;
-	next: null;
-	previous: null;
+	next: string | null;
+	previous: string | null;
 	results: PokeData[];
 }
 
@@ -15,20 +15,26 @@ interface PokeData {
 
 export default function Container() {
 	const [pokedata, setPokeData] = useState<PokeData[]>([]);
+	const [page, setPage] = useState(0);
+
+	// const dataSort = (data: PokeApiData) => {
+	// 	data.results.sort((a, b) => {
+	// 		const nameA = a.name.toLowerCase();
+	// 		const nameB = b.name.toLowerCase();
+
+	// 		if (nameA < nameB) return -1;
+	// 		if (nameA > nameB) return 1;
+	// 		return 0;
+	// 	});
+	// 	return data;
+	// };
 
 	const getData = async () => {
 		const response = await fetch(
-			"https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+			`https://pokeapi.co/api/v2/pokemon?limit=25&offset=${page}`
 		);
 		const data: PokeApiData = await response.json();
-		data.results.sort((a, b) => {
-			const nameA = a.name.toLowerCase();
-			const nameB = b.name.toLowerCase();
-
-			if (nameA < nameB) return -1;
-			if (nameA > nameB) return 1;
-			return 0;
-		});
+		// dataSort(data);
 		setPokeData(data.results);
 	};
 
@@ -37,9 +43,9 @@ export default function Container() {
 	}, []);
 
 	return (
-		<div className="bg-white text-black flex flex-wrap gap-4">
+		<div className="text-black flex flex-wrap gap-4">
 			{pokedata.map((data) => (
-				<Card key={data.name} name={data.name} />
+				<Card key={data.name} name={data.name} url={data.url} />
 			))}
 		</div>
 	);
