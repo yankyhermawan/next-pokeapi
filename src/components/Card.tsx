d103493dd2e346eff2093c70053cea9e8db593f3
yankyhermawan <yankyhermawan42@gmail.com>
2023-09-13 20:29:07 +0700

    Feat: Create filter content - Use input value to filter content - Combine filter content with pagination function

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,61 @@
 import { useState, useEffect, useMemo } from "react";
 import noImageIcon from "../no-image-icon.png";
-export default function Card({ name, url }: { name: string; url: string }) {
+
+interface Types {
+	slot: number;
+	type: {
+		name: string;
+		url: string;
+	};
+}
+
+interface Sprites_Other {
+	dream_world: {
+		front_default: string;
+		front_female: string | null;
+	};
+	home: {
+		front_default: string;
+		front_female: string | null;
+		front_shiny: string;
+		front_shiny_female: string | null;
+	};
+	"official-artwork": {
+		front_default: string;
+		front_shiny: string | null;
+	};
+}
+
+interface Sprites {
+	back_default: string;
+	back_female: string | null;
+	back_shiny: string;
+	back_shiny_female: string | null;
+	front_default: string;
+	front_female: string | null;
+	front_shiny: string;
+	front_shiny_female: string | null;
+	other: Sprites_Other;
+}
+
+interface DataFetched {
+	abilities: string[];
+	base_experience: number;
+	forms: string[];
+	game_indices: string[];
+	height: number;
+	held_items: string[];
+	id: number;
+	is_default: boolean;
+	location_area_encounters: string;
+	moves: string[];
+	name: string;
+	order: number;
+	species: string[];
+	sprites: Sprites;
+	types: Types[];
+}
+export default function Card({ name }: { name: string }) {
 	const [bgColor, setBGColor] = useState("");
 	const [id, setID] = useState(0);
 	const [bgURL, setBGURL] = useState("");
@@ -29,23 +84,46 @@ export default function Card({ name, url }: { name: string; url: string }) {
 	);
 
 	useEffect(() => {
-		// Initialize the ID when the component mounts
-		setID(Number(url.split("/")[url.split("/").length - 2]));
-	}, [url]);
-
-	useEffect(() => {
 		// Fetch the type and set the background URL and color
 		const getID = async () => {
 			const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-			const data = await response.json();
+			const data: DataFetched = await response.json();
 			setID(Number(data.id));
-			setBGURL(
-				`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`
-			);
+			if (data.sprites.other.home.front_default) {
+				setBGURL(data.sprites.other.home.front_default);
+			} else {
+				setBGURL(data.sprites.front_default);
+			}
+
 			setBGColor(typeColor[data.types[0].type.name]);
 		};
 		getID();
 	}, [id, name, typeColor]);
+
+	function getContrastColor(backgroundColor: string) {
+		// Parse the background color into RGB components
+		const hexColor = backgroundColor.replace(/^#/, ""); // Remove leading '#'
+		const r = parseInt(hexColor.slice(0, 2), 16);
+		const g = parseInt(hexColor.slice(2, 4), 16);
+		const b = parseInt(hexColor.slice(4, 6), 16);
+
+		// Calculate the relative luminance using the formula for sRGB
+		const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+		// Choose a text color based on the luminance
+		const textColor = luminance > 0.5 ? "#000" : "#fff"; // Dark background, light text; Light background, dark text
+		return textColor;
+	}
+
+	function titleCase(str: string) {
+		return str
+			.toLowerCase()
+			.split(" ")
+			.map(function (word) {
+				return word.charAt(0).toUpperCase() + word.slice(1);
+			})
+			.join(" ");
+	}
 
 	function convertToThreeDigitString(number: number) {
 		return "#" + String(number).padStart(3, "0");
@@ -63,7 +141,12 @@ export default function Card({ name, url }: { name: string; url: string }) {
 				}}
 				href={`https://pokeapi.co/api/v2/pokemon/${id}`}
 			>
-				<span className="text-lg p-2 text-white">
+				<span
+					className="text-lg p-2"
+					style={{
+						color: getContrastColor(bgColor),
+					}}
+				>
 					{convertToThreeDigitString(id)}
 				</span>
 				<img
@@ -74,7 +157,7 @@ export default function Card({ name, url }: { name: string; url: string }) {
 						e.currentTarget.src = noImageIcon; // Replace with the local image URL
 					}}
 				/>
-				<span className="text-lg p-2 text-white">{name}</span>
+				<span className="text-lg p-2 text-white">{titleCase(name)}</span>
 			</a>
 		</>
 	);
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -16,19 +16,24 @@ interface PokeData {
 }
 
 export default function Container() {
-	const [pokedata, setPokeData] = useState<PokeData[]>([]);
+	const defaultPokeApiData = {
+		count: 0,
+		next: null,
+		previous: null,
+		results: [],
+	};
+	const [pokedata, setPokeData] = useState<PokeApiData>(defaultPokeApiData);
+	const [dataToShow, setDataToShow] = useState<PokeData[]>([]);
 	const { pageNumber } = useParams();
 	const [totalPages, setTotalPages] = useState(0);
+	const [searchValue, setSearchValue] = useState("");
 
 	const getData = async () => {
 		const response = await fetch(
-			`https://pokeapi.co/api/v2/pokemon?limit=54&offset=${
-				(Number(pageNumber) - 1) * 54
-			}`
+			`https://pokeapi.co/api/v2/pokemon?limit=10000`
 		);
 		const data: PokeApiData = await response.json();
-		setPokeData(data.results);
-		setTotalPages(Math.ceil(Number(data.count) / 54));
+		setPokeData(data);
 	};
 	const getPageNumbers = () => {
 		const pageNumbers = [];
@@ -38,15 +43,50 @@ export default function Container() {
 		return pageNumbers;
 	};
 
+	const paginationData = (data: PokeData[]) => {
+		const itemsPerPage = 56;
+		const startIndex = (Number(pageNumber) - 1) * itemsPerPage;
+		const endIndex = startIndex + itemsPerPage;
+		const currentPageData = data.slice(startIndex, endIndex);
+		return currentPageData;
+	};
+
+	const filterData = () => {
+		if (!searchValue) {
+			setDataToShow(paginationData(pokedata.results));
+			setTotalPages(Math.ceil(Number(pokedata.results.length) / 56));
+		} else {
+			const filteredData = pokedata.results.filter((pokemon) => {
+				return pokemon.name.toLowerCase().includes(searchValue.toLowerCase());
+			});
+			const paginatedData = paginationData(filteredData);
+			setDataToShow(paginatedData);
+			setTotalPages(Math.ceil(Number(paginatedData.length) / 56));
+		}
+	};
+
 	useEffect(() => {
 		getData();
 	}, []);
+	useEffect(() => {
+		// Run filterData when pokedata or searchValue changes
+		filterData();
+	}, [pokedata, searchValue]);
+	const setInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchValue(e.target.value);
+	};
 
 	return (
-		<>
-			<div className="text-black flex flex-wrap gap-4 h-max justify-center">
-				{pokedata.map((data) => (
-					<Card key={data.name} name={data.name} url={data.url} />
+		<div className="flex flex-col items-center">
+			<input
+				type="text"
+				className="px-8 py-2 border-black border-solid border-[1px] rounded-full m-8 w-[50%] text-xl text-center"
+				value={searchValue}
+				onChange={setInput}
+			/>
+			<div className="text-black flex flex-wrap gap-4 h-max justify-center mx-auto">
+				{dataToShow.map((data) => (
+					<Card key={data.name} name={data.name} />
 				))}
 			</div>
 			<div className="flex flex-row m-8 justify-center gap-4">
@@ -55,6 +95,6 @@ export default function Container() {
 					totalPages={getPageNumbers()}
 				/>
 			</div>
-		</>
+		</div>
 	);
 }
